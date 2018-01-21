a826f9918d36690eb4d5e0913f624066f32725c8
Savitoj <savitojsingh15@gmail.com>
2018-01-21 19:49:23 +0530

    Contact form added

---
+++ contact.php
@@ -0,0 +1,18 @@
+<?php 
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$message = $_POST['message'];
+
+	$to = "savitojjaswal@gmail.com";
+	$subject = "New Message from ".$name;
+
+	$final_message = "Name: ".$name."\nEmail: ".$email."\nMessage: ".$message;
+
+	if(mail($to, $subject, $final_message, "From: ".$name)){
+		echo ("<p>Mail Sent.</p>");
+	}
+	else{
+		echo ("<p>Email delivery failed…</p>");
+	}
+
+?>
